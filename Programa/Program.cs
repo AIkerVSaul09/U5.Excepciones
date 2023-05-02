2567b9b87ab957fe665de736ceca4252b272e623
Iker Alonso <126517793+AIkerVSaul09@users.noreply.github.com>
2023-05-02 19:52:15 +0000

    Exepciones 02/05/2023

---
+++ Programa/Program.cs
@@ -0,0 +1,34 @@
+﻿ class Program
+{
+    private static void Main(string[] args)
+    {
+        uint? edad=0;
+        Console.WriteLine("Captura tu edad y presiona ºEnterº");
+        string? edadCaptuarada = Console.ReadLine();
+
+        if(edadCaptuarada is not null)
+        {
+            try
+            {
+                edad = uint.Parse(edadCaptuarada);
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Formato Incorrecto");
+            }
+             catch(System.OverflowException)
+            {
+                Console.WriteLine("La edad es muy grande o muy pequeña");
+            }
+        }
+        if(edad>=0)
+        {
+            Console.WriteLine($"Tu edad es  {edad}");
+        }
+        else
+        {
+            Console.WriteLine("Reingrese el resultado con un numero Positivo");
+        }
+
+    }
+}
